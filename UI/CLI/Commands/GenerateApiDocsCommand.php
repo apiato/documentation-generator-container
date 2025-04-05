55324543a314ce1831bbd4e7ee2fe3ebf7f7f19e
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-04-05 13:23:49 +0330

    fix wrong namespace

--- UI/CLI/Commands/GenerateApiDocsCommand.php
+++ UI/CLI/Commands/GenerateApiDocsCommand.php
@@ -3,7 +3,7 @@
 namespace App\Containers\Vendor\Documentation\UI\CLI\Commands;
 
 use App\Containers\Vendor\Documentation\Actions\GenerateDocumentationAction;
-use Apiato\Core\Commands\Command as AbstractConsoleCommand;
+use Apiato\Core\Console\Command as AbstractConsoleCommand;
 
 class GenerateApiDocsCommand extends AbstractConsoleCommand
 {
